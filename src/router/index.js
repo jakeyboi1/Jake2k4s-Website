4dcb2ee66e340373721d901ee1768691a6724710
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2024-01-21 16:03:02 -0800

    Change to hash router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import Home from '../pages/homePage.vue'
 import About from '../pages/aboutPage.vue'
 import Projects from '../pages/projectsPage.vue'
 
 const router = createRouter({
-    history: createWebHistory('/jake2k4s-website/'),
+    history: createWebHashHistory('/jake2k4s-website/'),
     routes: [
         { path: '/', name: "Home", component: Home },
         { path: '/about', name: "About", component: About },
