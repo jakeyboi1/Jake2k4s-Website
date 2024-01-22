dfcd321e570a42f03ac1a437235e6a77e55463f6
Jake Landrum <jakelandrum2004@gmail.com>
2024-01-22 09:44:24 -0600

    aboutSiteFix

--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -27,7 +27,7 @@ const warningVisible = ref(true)
             </div>
         </div>
 
-        <div class="w-96 h-34 ml-auto lg:pr-8 lg:pl-2"> <!--ml-auto puts it all the way to the left and helps mobile resolution using pr instead of px as its not in the center of page-->
+        <div class="w-96 h-34 ml-auto sm:pr-8 sm:pl-2"> <!--ml-auto puts it all the way to the left and helps mobile resolution using pr instead of px as its not in the center of page-->
             <div class="text-2xl font-bold text-red-500">About This Website!</div>
             <body class="text-white mt-2 text-xl">This website was created using Vue.js and Tailwind CSS in order to learn both frameworks. This site also houses all my projects I have worked on, and acts as my portfolio!</body>
         </div>
